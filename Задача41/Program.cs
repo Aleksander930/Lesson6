669724fa6843e3524e4116d50485316fff9c15c1
Alexander Prilepin <aleksander930@list.ru>
2022-10-25 02:02:23 +0300

    Lesson6

---
+++ Задача41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите M чисело: ");
+int Array = Convert.ToInt32(Console.ReadLine());
+
+int Numbers(int number, int arr)
+{
+    int counter = default;
+    for (int i = 0; i < arr; i++)
+    {
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > number) counter++;
+    }
+    return counter;
+}
+int arrnum = Numbers(0, Array);
+Console.WriteLine($"Ответ| {arrnum}");
---
+++ Задача43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("Введите b1: ");
+double b1 = Convert.ToUInt32(Console.ReadLine());
+System.Console.Write("Введите k1: ");
+double b2 = Convert.ToUInt32(Console.ReadLine());
+System.Console.Write("Введите b2: ");
+double k1 = Convert.ToUInt32(Console.ReadLine());
+System.Console.Write("Введите k2: ");
+double k2 = Convert.ToUInt32(Console.ReadLine());
+double x = (k1 - b1) / (b2 - k2);
+double y = b2 * x + b1;
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+System.Console.Write($" {x}, {y}");
